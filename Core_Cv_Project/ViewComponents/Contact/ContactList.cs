2c309f4c2eba30a6e51dd559856c280f3283cbeb
Symbytex <cagrisntrk0614@gmail.com>
2023-03-23 18:21:52 +0300

    .

---
+++ Core_Cv_Project/ViewComponents/Contact/ContactList.cs
@@ -0,0 +1,17 @@
+﻿using BussinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Core_Cv_Project.ViewComponents.Contact
+{
+    public class ContactList:ViewComponent
+    {
+        ContactManager _contactmanager = new ContactManager(new EfContactDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = _contactmanager.TGetList();
+            return View(values);
+        }
+    }
+}
---
+++ Core_Cv_Project/ViewComponents/Contact/SendMessage.cs
@@ -0,0 +1,20 @@
+﻿using BussinesLayer.Concrete;
+using EntityLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using DataAccessLayer.Concrete;
+using System.Xml.Linq;
+namespace Core_Cv_Project.ViewComponents.Contact
+{
+
+    public class SendMessage:ViewComponent
+    {
+  
+        public IViewComponentResult Invoke()
+        {     
+           
+            return View();  
+        }
+    }
+}
