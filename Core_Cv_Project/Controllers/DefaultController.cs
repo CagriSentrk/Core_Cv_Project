ce124223e590c93916a0d80a2e1b2aadb8eb93ef
Symbytex <cagrisntrk0614@gmail.com>
2023-03-25 22:47:07 +0300

    .

--- Core_Cv_Project/Controllers/DefaultController.cs
+++ Core_Cv_Project/Controllers/DefaultController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BussinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core_Cv_Project.Controllers
 {
+    
     public class DefaultController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult SendMessage(Contact c)
+        {
+            ContactManager contactmanager = new ContactManager(new EfContactDal());
+            contactmanager.TAdd(c);
+            return PartialView();      //düzelt burayı.(Mesajı gönderince Partiala gidiyor direkt.)
         }
     }
 }
