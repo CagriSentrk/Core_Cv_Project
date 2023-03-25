9de5d5bb5d17d90da7da40e0cc9f67c9db8fffaa
Symbytex <cagrisntrk0614@gmail.com>
2023-03-25 23:41:24 +0300

    .

---
+++ Core_Cv_Project/Controllers/JobController.cs
@@ -0,0 +1,22 @@
+﻿using BussinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Core_Cv_Project.Controllers
+{
+    public class JobController : Controller
+    {
+        JobManager jobManager = new JobManager(new EfJobDal());
+        public IActionResult Index(int id)
+        {
+            var values = jobManager.TGetList().Where(x=>x.Job_Id==id).ToList();
+            return View(values);
+        }
+        public IActionResult Deneme()
+        {
+            return View();
+        }
+    }
+
+}
