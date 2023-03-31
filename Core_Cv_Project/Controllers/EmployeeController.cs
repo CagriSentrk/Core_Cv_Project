3cbdf3a7f7f03e523bfb75836c89aa8de8b7b423
Symbytex <cagrisntrk0614@gmail.com>
2023-04-01 00:56:37 +0300

    .

---
+++ Core_Cv_Project/Controllers/EmployeeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using System;
+using BussinesLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+
+namespace Core_Cv_Project.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult ImageUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FileUpload(IFormFile formFile,Employee model )
+        {
+            if (formFile != null)
+            { 
+
+                var extent = Path.GetExtension(formFile.FileName);
+                var randomName = ($"{Guid.NewGuid()}{extent}");
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img", randomName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await formFile.CopyToAsync(stream);
+                }
+                model.Content = randomName;
+                EmployeeManager employeeManager = new EmployeeManager(new EfEmployeeDal());
+                employeeManager.TAdd(model);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> FileUpload(Employee model, IFormFile formFile)
+        {
+            if (formFile != null)
+            {
+                var extent = Path.GetExtension(formFile.FileName);
+                var randomName = ($"{Guid.NewGuid()}{extent}");
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img", randomName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await formFile.CopyToAsync(stream);
+                }
+
+                model.Content = randomName;
+                EmployeeManager employeeManager = new EmployeeManager(new EfEmployeeDal());
+                employeeManager.TAdd(model);
+            }
+
+            return View();
+        }
+    }
+}
--- Core_Cv_Project/Controllers/JobController.cs
+++ Core_Cv_Project/Controllers/JobController.cs
@@ -2,8 +2,15 @@
 using DataAccessLayer.Abstract;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Core_Cv_Project.Controllers
 {
@@ -13,18 +20,47 @@ namespace Core_Cv_Project.Controllers
         public IActionResult Index(int id)
         {
             ViewBag.v2 = id;
-            var values = jobManager.TGetList().Where(x=>x.Job_Id==id).ToList();
+            var values = jobManager.TGetList().Where(x => x.Job_Id == id).ToList();
             return View(values);
         }
-   
+
         public PartialViewResult SendCv(Employee c)
         {
-            
+
 
             EmployeeManager employeeManager = new EmployeeManager(new EfEmployeeDal());
             employeeManager.TAdd(c);
             return PartialView();
         }
+        [HttpGet]
+        public IActionResult ImageUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FileUpload(IFormFile formFile,Employee model)
+        {
+            if (formFile != null)
+            { 
+
+                var extent = Path.GetExtension(formFile.FileName);
+                var randomName = ($"{Guid.NewGuid()}{extent}");
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img",randomName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await formFile.CopyToAsync(stream);
+                }
+                EmployeeManager employeeManager = new EmployeeManager(new EfEmployeeDal());
+                model.Cv = randomName;
+                employeeManager.TAdd(model);
+
+            }
+            return View();
+        }
+
+
 
     }
 
